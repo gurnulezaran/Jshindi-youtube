4768f7980b50f404a31f12939c4b6fa7aab4de48
Zaran Gurnule <107485486+gurnulezaran@users.noreply.github.com>
2023-09-27 18:53:14 +0000

    variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144563
+let accountEmail = "zaran@googl.com"
+var accountPassword ="123456"
+accountCity = "Vapi"
+let accountState;
+
+
+
+
+accountEmail ="zg@zg.com"
+accountPassword = "23466638"
+accountCity ="Ahemdabad"
+
+console.log(accountId)
+
+
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity])
