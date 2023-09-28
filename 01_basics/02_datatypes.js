4aad6855e5af7bbf308468e16087e589b9a8c64c
Zaran Gurnule <107485486+gurnulezaran@users.noreply.github.com>
2023-09-28 11:04:27 +0000

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,49 @@
+"use strict";//treat all JS codeas newer version
+
+//alert( 3 + 3 )  // we are using nodejs,not browser
+
+console.log(3 
+    +3)
+//code readability should be  high
+
+    console.log("Zimesh")
+
+
+    let name = "Zimesh"
+    let age = 18
+    let isLoggedIn = false
+    let strate;
+
+
+    //number => 2 to power 53
+    //bigint
+    //string =>""
+    //boolean => true/false
+    // null => standloan value
+    // undefined => 
+    //symbol=> unique
+
+    //poject
+
+
+    console.log(typeof undefined); //underfined
+    console.log(typeof null);// object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
